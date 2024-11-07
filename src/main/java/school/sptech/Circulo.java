61a7c64eb285671f4844e9ddf231fca14f8e2e52
Rafaelsmlee <160335315+Rafaelsmlee@users.noreply.github.com>
2024-11-07 18:41:40 -0300

    Add: ajuste

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,31 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+    private Double raio;
+
+    public Circulo() {}
+
+    public Circulo(Double raio, String cor, Integer espessura) {
+        super(cor, espessura);
+        this.raio = raio;
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    @Override
+    public String toString() {
+        return "Raio = " + raio +
+                ", área = " + calcularArea();
+    }
 }
